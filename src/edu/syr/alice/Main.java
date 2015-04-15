f8ce0b9bdd0f015ab8d1bc8382b2c67df07c5919
Alice Yu <zyu05@syr.edu>
2015-04-15 12:25:17 -0400

    ~

--- src/edu/syr/alice/Main.java
+++ src/edu/syr/alice/Main.java
@@ -1,13 +1,13 @@
 package edu.syr.alice;
 
 
+
 /**
  * Created by Alice Yu on 3/22/15.
  * Email: zyu05@syr.edu
  */
 
 public class Main {
-
     public static void main(String[] args) {
     }
 }
