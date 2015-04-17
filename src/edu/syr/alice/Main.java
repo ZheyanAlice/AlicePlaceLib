7867b78d22c2c84974e272b405648e235ae8ec7f
Alice Yu <zyu05@syr.edu>
2015-04-16 22:49:26 -0400

    Mod:Main.java

--- src/edu/syr/alice/Main.java
+++ src/edu/syr/alice/Main.java
@@ -1,13 +1,18 @@
 package edu.syr.alice;
 
 
+import edu.syr.alice.authentication.KeyBundle;
+import edu.syr.alice.place.PlaceSearchClient;
+
 /**
  * Created by Alice Yu on 3/22/15.
  * Email: zyu05@syr.edu
  */
 
 public class Main {
-
     public static void main(String[] args) {
+        KeyBundle keyBundle = new KeyBundle();
+        PlaceSearchClient placeSearchClient = new PlaceSearchClient(keyBundle);
+
     }
 }
